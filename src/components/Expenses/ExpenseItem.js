27b1ef447844dedf9fa107d039e46ae5d65b4023
Ling Fung Chan <justinlfchan@gmail.com>
2023-01-28 14:27:09 +0000

    added useState() for updating dynmaic data

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,26 @@
+import React, { useState } from "react";
+
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("New item");
+    console.log(title);
+  };
+
   return (
     <Card className="expense-item">
       {/* <div>{props.date.toISOString()}</div> */}
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
       </div>
       <div className="expense-item__price">${props.amount}</div>
+      <button onClick={clickHandler}>Click</button>
     </Card>
   );
 }
