9d92301cc20749a7a4c5251c81be82e4dabf2a3b
Ling Fung Chan <justinlfchan@gmail.com>
2023-01-28 17:23:12 +0000

    added useState() and title/amount/dateChangeHandler for form input

--- src/components/NexExpense/ExpenseForm.js
+++ src/components/NexExpense/ExpenseForm.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
   const titleChangeHandler = (e) => {
+    setEnteredTitle(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const amountChangeHandler = (e) => {
+    setEnteredAmount(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const dateChangeHandler = (e) => {
+    setEnteredDate(e.target.value);
     console.log(e.target.value);
   };
 
@@ -16,11 +31,21 @@ const ExpenseForm = () => {
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" />
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
